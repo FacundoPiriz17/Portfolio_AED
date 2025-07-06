4913a5c72b274de6d501d498812100f94957a68f
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2025-07-06 13:09:43 -0300

    a

---
+++ Primer Parcial Preparacion/Tads-Definitivos1/ArbolAVL/src/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+    public static void main(String[] args) {
+        ArbolAVL<Integer> prueba = new ArbolAVL<>();
+        prueba.insertar(57,57);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(28,28);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(12,12);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(35,35);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(96,96);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(76,76);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(42,42);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(7,7);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(31,31);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(53,53);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(104,104);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(30,30);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(20,20);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(61,61);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(84,84);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+        prueba.insertar(87,87);
+        System.out.println(prueba.preOrden());
+        System.out.println("---------------------");
+    }
+}
---
+++ Primer Parcial Preparacion/Tads-Definitivos1/Conjuntos/src/IConjunto.java
@@ -0,0 +1,7 @@
+public interface IConjunto<T> extends ILista<T> {
+    public void insertar(Nodo<T> unNodo);
+    public IConjunto<T> union(IConjunto<T> otroConjunto);
+    public IConjunto<T> interseccion(IConjunto<T> otroConjunto);
+    public IConjunto<T> diferenciaSimetrica(IConjunto otroConjunto);
+    public IConjunto<T> complemento(IConjunto<T> conjuntoUniversal);
+}
